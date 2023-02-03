661b00cda64e81e83cdd54add7fc7bea611d142f
Greg Pauloski <18683347+gpauloski@users.noreply.github.com>
2023-02-03 17:18:23 -0600

    Add distributed initialization

---
+++ tests/initialize_test.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+import os
+import pathlib
+from typing import Generator
+from unittest import mock
+
+import pytest
+
+from llm.initialize import get_default_parser
+from llm.initialize import initialize
+from llm.initialize import initialize_from_args
+
+
+@pytest.fixture
+def distributed() -> Generator[None, None, None]:
+    with (
+        mock.patch('torch.distributed.init_process_group'),
+        mock.patch('torch.distributed.get_backend', return_value=True),
+        mock.patch('torch.distributed.get_world_size', return_value=1),
+    ):
+        yield
+
+
+def test_initialize_from_args(distributed, tmp_path: pathlib.Path) -> None:
+    config_str = 'a = 1'
+    config_path = tmp_path / 'config.py'
+    with open(config_path, 'w') as f:
+        f.write(config_str)
+
+    parser = get_default_parser()
+    args = parser.parse_args(['--config', str(config_path), '--debug'])
+
+    config = initialize_from_args(args)
+    assert config.a == 1
+    assert os.environ['LOCAL_RANK'] == '0'
+    del os.environ['LOCAL_RANK']
+
+
+@pytest.mark.parametrize('cuda', (True, False))
+@mock.patch.dict(os.environ, {'LOCAL_RANK': '0'})
+def test_initialize_cuda(cuda, distributed) -> None:
+    with (
+        mock.patch('torch.cuda.is_available', return_value=cuda),
+        mock.patch('torch.cuda.set_device') as mock_device,
+    ):
+        initialize()
+        assert mock_device.called == cuda
