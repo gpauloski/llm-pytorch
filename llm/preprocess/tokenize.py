fede9834e1c08c496cab372806f5a572088fe6fb
Greg Pauloski <18683347+gpauloski@users.noreply.github.com>
2023-03-14 18:11:59 -0500

    Add padding/truncation to tokenizers

--- llm/preprocess/tokenize.py
+++ llm/preprocess/tokenize.py
@@ -10,10 +10,12 @@ def get_tokenizer(
     kind: Literal['wordpiece', 'bpe'],
     vocab: dict[str, int] | str | None = None,
     lowercase: bool = False,
+    padding_options: dict[str, Any] | None = None,
+    truncation_options: dict[str, Any] | None = None,
     **kwargs: Any,
 ) -> tokenizers.implementations.base_tokenizer.BaseTokenizer:
     if kind == 'wordpiece':
-        return tokenizers.BertWordPieceTokenizer(
+        tokenizer = tokenizers.BertWordPieceTokenizer(
             vocab=vocab,
             clean_text=True,
             handle_chinese_chars=True,
@@ -21,7 +23,7 @@ def get_tokenizer(
             **kwargs,
         )
     elif kind == 'bpe':
-        return tokenizers.ByteLevelBPETokenizer(
+        tokenizer = tokenizers.ByteLevelBPETokenizer(
             vocab=vocab,
             lowercase=lowercase,
             add_prefix_space=True,
@@ -30,3 +32,10 @@ def get_tokenizer(
         )
     else:
         raise AssertionError(f'Unsupported kind "{kind}."')
+
+    if padding_options is not None:
+        tokenizer.enable_padding(**padding_options)
+    if truncation_options is not None:
+        tokenizer.enable_truncation(**truncation_options)
+
+    return tokenizer
--- tests/preprocess/tokenize_test.py
+++ tests/preprocess/tokenize_test.py
@@ -14,3 +14,15 @@ def test_tokenizer(kind: Literal['bpe', 'wordpiece']) -> None:
         get_tokenizer(kind),
         tokenizers.implementations.base_tokenizer.BaseTokenizer,
     )
+
+
+@pytest.mark.parametrize('kind', ('bpe', 'wordpiece'))
+def test_tokenizer_with_max_seq_len(kind: Literal['bpe', 'wordpiece']) -> None:
+    assert isinstance(
+        get_tokenizer(
+            kind,
+            padding_options={'length': 128},
+            truncation_options={'max_length': 128},
+        ),
+        tokenizers.implementations.base_tokenizer.BaseTokenizer,
+    )
